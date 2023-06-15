43d6846e7b004c22494bbb58e6992f8358b12c10
Komfort Kimko <komfortkimko@gmail.com>
2023-06-15 11:45:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to the output
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
