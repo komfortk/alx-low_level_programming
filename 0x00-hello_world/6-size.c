4b2be77ce975129ea8b788065fd03702db77f16e
Komfort Kimko <komfortkimko@gmail.com>
2023-06-15 12:57:56 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void) 
 {
-    printf("Size of char: %lu byte(s)\n", sizeof(char));
-    printf("Size of int: %lu byte(s)\n", sizeof(int));
-    printf("Size of float: %lu byte(s)\n", sizeof(float));
-    printf("Size of long: %lu byte(s)\n", sizeof(long));
-    printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-    return (0);
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+return (0);
 }
