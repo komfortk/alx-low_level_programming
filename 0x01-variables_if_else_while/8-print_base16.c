fc8cd1f6dbf82e93957bd80c655f41b0518eb6a2
Komfort Kimko <komfortkimko@gmail.com>
2023-06-16 12:18:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers of base 16 in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
