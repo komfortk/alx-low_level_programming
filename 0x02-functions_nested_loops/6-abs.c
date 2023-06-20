fee42c0e60268fe1f3bdb0c98c207e35a11c12ff
Komfort Kimko <komfortkimko@gmail.com>
2023-06-20 10:06:56 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute value of an integer
+ * @i: The integer to be checked
+ * Return: The absolute value of the integer
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
