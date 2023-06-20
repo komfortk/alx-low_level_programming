c31df6d12973d979946f640a0bd2eedf39bb581f
Komfort Kimko <komfortkimko@gmail.com>
2023-06-20 13:27:56 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @r: The first integer to be computed
+ * @s: The second integer to be computed
+ * Return: Integer value of the sum
+ */
+int add(int r, int s)
+{
+	return (r + s);
+}
