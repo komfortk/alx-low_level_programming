c68a6272497646602abfb8846059354e4b7255a0
Komfort Kimko <komfortkimko@gmail.com>
2023-06-20 17:55:41 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,11 +18,11 @@ void print_times_table(int n)
 
 				if (j == 0)
 				{
-					printf("%d", product);
+					printf("%-4d", product);
 				}
 				else
 				{
-					printf(",   %d", product);
+					printf(", %4d", product);
 				}
 			}
 			printf("\n");
