0a8d716b0947ca37859691453bc8425963fa863b
Komfort Kimko <komfortkimko@gmail.com>
2023-06-22 11:21:01 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints the numbers 1 to 100
+ * Fizz for multiples of 3, Buzz for multiples of 5
+ * FizzBuzz for multiples of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+	printf("\n");
+	return (0);
+}
