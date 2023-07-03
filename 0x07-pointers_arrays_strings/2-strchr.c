43820b4803b95783a5b044e580692487b8175aca
Komfort Kimko <komfortkimko@gmail.com>
2023-07-03 09:57:52 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: input string
+ * @c: character to be located
+ * Return: pointer to the first occurrence of c in string s
+ * Null if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] == c)
+			return (&s[index]);
+	}
+	return ('\0');
+}
