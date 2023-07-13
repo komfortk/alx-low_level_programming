544a7c933d9719f849d442e3136b91299a0f9404
Komfort Kimko <komfortkimko@gmail.com>
2023-07-13 09:39:14 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: nothing
+ * Return: pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
