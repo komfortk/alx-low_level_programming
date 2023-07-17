5fc6597ba8f2255b01e072f182998784800e1045
Komfort Kimko <komfortkimko@gmail.com>
2023-07-17 13:36:59 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's basic information
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
