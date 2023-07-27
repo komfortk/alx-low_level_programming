e0376a0ddf1c38c669fe4def6382ffe16e2c6078
komfortk <komfortkimko@gmail.com>
2023-07-27 11:20:17 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list to print
+ *
+ * Return: the number of elements printed, [0] (nil) if str is NULL
+ */
+size_t print_list(const list_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		k++;
+	}
+
+	return (k);
+}
